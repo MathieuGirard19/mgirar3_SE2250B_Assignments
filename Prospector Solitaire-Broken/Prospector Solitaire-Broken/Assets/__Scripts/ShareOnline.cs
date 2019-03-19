6d55139bfddc120d0bfeb544c1f5812491b9732c
MathieuGirard <mathieu.m.girard@gmail.com>
2019-03-19 10:26:41 -0400

    final commit

---
+++ Prospector Solitaire-Broken/Prospector Solitaire-Broken/Assets/__Scripts/ShareOnline.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShareOnline : MonoBehaviour
+{
+    private string _twitterUrl = "http://twitter.com/intent/tweet";
+    private string _twitterLanguage = "en";
+    private string _twitterDisplay = "My Solitaire High Score: ";
+
+    public void ShareScore()
+    {
+        Application.OpenURL(_twitterUrl + "?text=" + WWW.EscapeURL(_twitterDisplay) + ScoreManager.HIGH_SCORE + "&amp;lang=" + WWW.EscapeURL(_twitterLanguage));
+    }
+}
